6f67557c47595fb9f1c86b9d1cddd6725ac8beb0
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-08-23 19:49:47 +0300

    Complete fourth task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    private tenants: Array<Person>;
+    protected key: Key;
+    protected door: boolean;
+
+    constructor(key: Key) {
+        this.key = key;
+        this.door = false;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    public openDoor(key: Key): void {
+        if (key === this.key) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
